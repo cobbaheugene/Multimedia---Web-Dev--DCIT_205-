eedfb861ac982122f94b83e3102ccf157e7a39ef
cobbaheugene <cobbaheugene@gmail.com>
2022-04-01 11:36:39 +0000

    a little to do with conditionals and loops

--- trial.js
+++ trial.js
@@ -23,7 +23,18 @@ else{
     console.log("No condition was met!")
 }
 
-//Switch is another conditional stement for testing for multiple statement 
+// Switch is another conditional stement for testing for multiple statement 
 // **assignment** read on switches and write a program
 
-//=== is not only used to test for values but to test for data types as well
+// === is not only used to test for values but to test for data types as well
+
+// loops repeat themselves
+//in the for loop, 3 parameters involved.
+
+// A PROGRAM TO LIST NUMBERS FROM 1 TO 5.. INCREMENTAL 
+for (let i=0; i<5; i++){
+    console.log(i+1)
+}
+
+//without the increment value(i++) you have an infinite loop wich will run saaaaa
+//i++ holds the value of the i and adds 1 to the value of i and reassigns it to it to run
