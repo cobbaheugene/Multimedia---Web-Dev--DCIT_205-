1d21da56ab4897f70a39d47d97fc7c1d7df252a4
cobbaheugene <cobbaheugene@gmail.com>
2022-04-01 11:06:55 +0000

    initial commit

---
+++ trial.js
@@ -0,0 +1,16 @@
+// Conditional Statements //
+
+//Declare the variable
+var x = 7
+
+if(x==7){
+    console.log("X is Seven")
+}
+else{
+    console.log("X is not Seven")
+}
+// To test for multiple conditons we use the keyword else if
+//they all terminate with an else statement
+
+//Switch is another conditional stement for testing for multiple statement 
+// **assignment** read on switches and write a program
