582a50cc88dcbaba9b615d858391f305ccbc3b4b
cobbaheugene <cobbaheugene@gmail.com>
2022-04-01 11:13:49 +0000

    update

--- trial.js
+++ trial.js
@@ -9,8 +9,21 @@ if(x==7){
 else{
     console.log("X is not Seven")
 }
+
 // To test for multiple conditons we use the keyword else if
 //they all terminate with an else statement
+var x=6
+if(x==4){
+    console.log("X is four")
+}
+else if(x==6){
+    console.log("X is Six")
+}
+else{
+    console.log("No condition was met!")
+}
 
 //Switch is another conditional stement for testing for multiple statement 
 // **assignment** read on switches and write a program
+
+//=== is not only used to test for values but to test for data types as well
