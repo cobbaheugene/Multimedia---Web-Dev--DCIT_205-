fdc4b672daa929c78ea30eb7d4874d5c05ae3474
cobbaheugene <cobbaheugene@gmail.com>
2022-04-08 11:08:46 +0000

    Switch cases

--- trial.js
+++ trial.js
@@ -23,8 +23,33 @@ else{
     console.log("No condition was met!")
 }
 
+// switch is used just like a conditional statement , that is , we use it to execute a block of code.
+// the code above simply enables us to print any of the cases 
+// our const was red so here it prints dislay red is not part of our case.
+// this means we give the code an option what to print .
+// if what we want to print is not part of the case, it prints out our default statement which is 'that is an animal'.
+
 // Switch is another conditional stement for testing for multiple statement 
 // **assignment** read on switches and write a program
+
+const colours = 'red';
+switch (colours) {
+    case 'pink':
+     console.log('Baby girls');;
+     break ;
+    case 'Blue' :
+     console.log('Chealse football teeam');
+     break ;
+    case 'black' :
+     console.log ('Official colour');
+     break ;
+    case 'dodger blue' :
+     console.log ('Tinas favorite colour')
+     break ;
+    default :
+     console.log('Display red');
+     break;
+}
 
 // === is not only used to test for values but to test for data types as well
 
